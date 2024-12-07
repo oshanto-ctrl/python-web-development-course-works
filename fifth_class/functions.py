581c4c65eb1b697d03b370cbfb6d8cfa2ebcefbc
Rejoan Siddiky <siddikyrejoan@gmail.com>
2024-12-07 21:02:28 +0600

    leaning functions

---
+++ fifth_class/functions.py
@@ -0,0 +1,60 @@
+''' Functions in python '''
+
+# function structure
+def function_name():
+    pass # something to do
+
+# arguments vs parameters
+'''
+def f(parameters...):
+    pass
+
+# call f()
+f(arguments...)
+'''
+def my_name():
+    return "MD Rejoan Siddiky"
+
+name = my_name()
+print(f"Your name is {name} and has {len(name)} characters in it!")
+
+
+# arg = "arguments"
+
+def my_function(*a):
+    s = sum(a)
+    print(f"Values of *a = {a}\ntype is {type(a)}") # constructs tuple
+    return s
+
+var = my_function(1, 10, 11)
+print(var)
+
+print("\n")
+
+# kwargs = "keyword arguments" # Passing arguments with keywords
+
+def my_function(**kwargs):
+    
+    for key, value in kwargs.items():
+        print(key, value)
+    
+    # print(f"Values of **kwargs {kwargs}\ntype is: {type(kwargs)}")
+    return kwargs
+
+var = my_function(name="John", age=22, location="dhaka, bgd", acctype="premium")
+print(var)
+
+# args and kwargs together
+def fishy(*args, **kwargs):
+    print("Printing from inside fishy func()")
+    print(args)
+    print(kwargs)
+    return args, kwargs
+
+# call fishy
+fish = fishy(1, 2, 3, 4, 5, name="yummy", location="pond", level="middle")
+# Unpack the fish information
+fish_tup, fish_dict = fish
+print(fish_tup) # tuple 1, 2, 3, 4, 5
+print(fish_dict) # dict{}
+
