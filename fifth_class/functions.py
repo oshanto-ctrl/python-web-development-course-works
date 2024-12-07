7f38bfa32387781757e7ba421e4dc4924927fcfa
Rejoan Siddiky <siddikyrejoan@gmail.com>
2024-12-07 21:07:57 +0600

    staring lambda functions and decorators

--- fifth_class/functions.py
+++ fifth_class/functions.py
@@ -45,6 +45,8 @@ var = my_function(name="John", age=22, location="dhaka, bgd", acctype="premium")
 print(var)
 
 # args and kwargs together
+# *args will go first as parameter
+# **kwargs will go second as parameter when using both.
 def fishy(*args, **kwargs):
     print("Printing from inside fishy func()")
     print(args)
@@ -58,3 +60,12 @@ fish_tup, fish_dict = fish
 print(fish_tup) # tuple 1, 2, 3, 4, 5
 print(fish_dict) # dict{}
 
+
+# Default parameter
+def users(name, age, location="BGD"):
+    info = f"Name: {name} {age} years old located in {location}"
+    return info
+
+me = users("abc", 22, )
+you = users("xyz", 23, "GB")
+print(f"{me}\n{you}")
