6f8df1c62c6d35e192f64a354c83a4f35dfb653e
Rejoan Siddiky <siddikyrejoan@gmail.com>
2024-12-22 21:35:16 +0600

    Assisgment 2 question on conditional requires user input

---
+++ oop_course/class_7_assignment.py
@@ -0,0 +1,27 @@
+'''
+Assignment 2
+
+You are tasked with creating a python program for a restaurant that
+offers different discounts based on the day and time of the week.
+Wreite a python script that prompots the user to input the day of the week
+and the time(in 24 hour format) they plan to visit the restaurant.
+Based on this information, the program should output the applicable discount offer.
+
+The discount offers are as follows:
+# On weekdays (Monday to Friday):
+Before 5 PM: No discount.
+After 5 PM: 10% discount on the total bill amount.
+
+# On weekends (Saturday and Sunday):
+Before 3 PM: 5% discount on the total bill amount.
+After 3 PM: 7% discount on the total bill amount.
+
+Your program should validate the user input to ensure it corresponds to valid days of the week
+and a valid time format. If the input is invalid, the program should
+display an appropriate error message and prompt the user to input again.
+
+Your program should use conditional statements to determine the discount offer
+based on the user input.
+
+Ensure your code is well documented and follows python best practices.
+'''
