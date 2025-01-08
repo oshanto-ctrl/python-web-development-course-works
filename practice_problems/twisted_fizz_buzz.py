3a729d54fde9f8f3117d09be2d199263ea46f08c
Rejoan Siddiky <siddikyrejoan@gmail.com>
2025-01-08 17:12:34 +0600

    Twisted_fizz_buz problem solve

--- practice_problems/twisted_fizz_buzz.py
+++ practice_problems/twisted_fizz_buzz.py
@@ -41,5 +41,37 @@ FizzBuzz
 3. Numbers divisible by 5 (but not 3) print "Buzz": 5,105, 105,10.
 4. Numbers divisible by both 3 and 5 print "FizzBuzz": 151515.
 """
+import math
+
+# Check if number is prime
+def is_prime(num):
+    if n < 2:
+        return False
+    i = 2
+    while (i*i) <= num:
+        if num%i == 0:
+            return False
+        i += 1
+    return True
+
+# Input N numbers
+# n = 15
+n = int(input())
+
+# For matching sample output
+print("0 and 1 is Not Prime, Fizz, Buzz or FizzBuzz.")
+
+for num in range(2, n+1):
+    # print(num, end=" ")
+    if is_prime(num):
+        print(num, " Prime")
+    elif (num%3 == 0) and (num%5 == 0):
+        print(num, " FizzBuzz")
+    elif (num%5 == 0) and (num%3 != 0):
+        print(num, " Buzz")
+    elif (num%3 == 0) and (num%5 != 0):
+        print(num, " Fizz")
+
+
 
 
