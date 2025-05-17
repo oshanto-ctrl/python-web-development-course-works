d41b2abee6db5d073e1859399f091d811c73434a
Rejoan Siddiky <siddikyrejoan@gmail.com>
2025-05-18 00:54:44 +0600

    oop class

--- fifteenth_class/oop.py
+++ fifteenth_class/oop.py
@@ -10,3 +10,10 @@ class Human:
 obj_1 = Human("Alice", 22)
 print(obj_1.name)
 obj_1.say_hello()
+
+# Constructor: What is constructor?
+"""
+__init__() method is the constructor.
+When we create a new object of a class, 
+this special function initialize attributes of the object.
+"""
