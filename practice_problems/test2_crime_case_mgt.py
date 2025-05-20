bc49e4202b4f51d6c0012842d8d20917965df2e1
Rejoan Siddiky <siddikyrejoan@gmail.com>
2025-05-20 12:20:28 +0600

    Finsihed the assignment on crime case management with python, completed all methods as required

--- practice_problems/test2_crime_case_mgt.py
+++ practice_problems/test2_crime_case_mgt.py
@@ -18,8 +18,9 @@ def retrive_cases():
             print("Suspects:")
             for suspect in details['Suspects']:
                 print(f"- {suspect}")
+            print()
         else:
-            print("Suspects: \nFound No Suspects For This Case Currently.")
+            print("Suspects: \nFound No Suspects For This Case Currently.\n")
     
     print("-" * 30)
 
@@ -100,8 +101,92 @@ def update_suspects():
     for suspect in updated_suspects:
         print(f"-   {suspect}")
 
+# Search Cases Status: 'Open'
+def search_open_cases():
+    print("\nOpen Cases:")
+    found = False
+    for case_id, details in cases.items():
+        if details["Status"].lower() == "open":
+            print(f"\nCase ID: {case_id}")
+            print(f"Title: {details["Title"]}")
+            print(f"Suspects: {', '.join(details["Suspects"]) if details["Suspects"] else "None"}")
+            found = True
+    if not found:
+        print("No Open Cases Found.")
+
+
+# Delete an existing case.
+def delete_case():
+    case_id = input("Enter Case ID to Delete: ").strip()
+    if case_id in cases:
+        confirm = input(f"Are you sure you want to delete case '{case_id}'? (yes/no)").lower()
+        if confirm == "yes":
+            del cases[case_id]
+            print(f"Case '{case_id}' has been deleted.")
+        else:
+            print("Case Deletion Was Canceled.")
+    else:
+        print(f"Case ID '{case_id}' not found!")
+
+
+# Find cases by suspect name
+def find_cases_by_suspect():
+    suspect_name = input("Enter suspect name: ").strip().lower()
+    found = False
+    print(f"Case involing suspect {suspect_name}")
+    
+    for case_id, details in cases.items():
+        if any(suspect_name == suspect.lower() for suspect in details['Suspect']):
+            print(f"\nCase ID: {case_id}")
+            print(f"\nCase Title: {details['Title']}")
+            print(f"Status: {details['Status']}")
+            print(f"Suspects: {', '.join(details['Suspects'])}")
+            found = True
+
+    if not found:
+            print(f"No cases involved with {suspect_name}")
+
+
 # Drivers
 # retrive_cases()
 # add_case()
 # update_status()
-update_suspects()
+# update_suspects()
+# search_open_cases
+# delete_case()
+# find_cases_by_suspect()
+
+while True:
+    print(f"\n--- Case Management ---")
+    print(f"1. All Cases")
+    print(f"2. Search 'OPEN' Cases")
+    print(f"3. Find by Suspect Name")
+    print(f"4. Add Case")
+    print(f"5. Update Suspect List")
+    print(f"6. Updated Case Status")
+    print(f"7. Delete Case")
+    print(f"8. Exit")
+
+    choice = input("Choose and option (1-8): ").strip()
+
+    match choice:
+        case "1":
+            retrive_cases()
+        case "2":
+            search_open_cases()
+        case "3":
+            find_cases_by_suspect()
+        case "4":
+            add_case()
+        case "5":
+            update_suspects()
+        case "6":
+            update_status()
+        case "7":
+            delete_case()
+        case "8":
+            print("Exiting program...")
+            break
+        case _:
+            print("Invalid choice. Please select 1-8.")
+        
