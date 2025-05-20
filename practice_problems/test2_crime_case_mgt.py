4410e6e0ccee37b5f47ad6b79e42ecdc5bfaf107
Rejoan Siddiky <siddikyrejoan@gmail.com>
2025-05-20 10:43:09 +0600

    Added 2 more function to the crime_case test 2 assignment

--- practice_problems/test2_crime_case_mgt.py
+++ practice_problems/test2_crime_case_mgt.py
@@ -23,12 +23,13 @@ def retrive_cases():
     
     print("-" * 30)
 
-# Add a new case to json file
+
+# Add a new case
 def add_case():
     # User's input of Case ID, Title, Suspects(list), status
     case_id = input("Enter Case ID: ").strip()
     case_title = input("Enter Title: ").strip()
-    case_suspects = input("Enter suspects (Comma between two suspects)").strip()
+    case_suspects = input("Enter suspects (Comma between two suspects): ").strip()
     status = input("Enter Case Status (Open/Close): ").strip().capitalize()
 
     # Make the suspect list
@@ -51,6 +52,56 @@ def add_case():
     
     print(f"Case {case_id} added successfully to Cases.")
 
+# Update Case Status
+def update_status():
+    valid_statuses = ["Open", "Under Investigation", "Closed"]
+    # Prompt User
+    case_id = input("Enter Case ID: ").strip()
+    new_status = input("Enter New Status (Open, Under Investigation, Closed): ").strip()
+
+    # Check for invalid new status
+    if new_status not in valid_statuses:
+        print("Invalid Status. Please Choose from Open, Under Investigation, or Closed.")
+        exit()
+    
+    # Update status if case exists already
+    if case_id in cases:
+        cases[case_id]["Status"] = new_status
+
+        with open("cases.json", 'w') as file:
+            json.dump(cases, file, indent=4)
+        
+        print(f"Case ID {case_id} status updated to '{new_status}'")
+    else:
+        print(f"Case ID '{case_id}' not found.")
+
+
+# Update Suspects to cases
+def update_suspects():
+    case_id = input("Enter Case ID to add susptects: ").strip()
+    if case_id not in cases:
+        print(f"Case ID: {case_id} not found!")
+        exit()
+    
+    new_suspects = input("Enter suspect names to add (Use comma between names): ").strip()
+    new_suspects_list = [suspect.strip() for suspect in new_suspects.split(",") if suspect.strip()]
+    
+    existing_suspects = set(cases[case_id].get("Suspects", []))
+    updated_suspects = list(existing_suspects.union(new_suspects_list))
+
+    # Update json file
+    cases[case_id]["Suspects"] = updated_suspects
+
+    # Save the file
+    with open("cases.json", 'w') as file:
+        json.dump(cases, file, indent=4)
+    print(f"Supsects updated to case {case_id}")
+    print("Updated suspects list:")
+    for suspect in updated_suspects:
+        print(f"-   {suspect}")
+
 # Drivers
-retrive_cases()
-add_case()
+# retrive_cases()
+# add_case()
+# update_status()
+update_suspects()
