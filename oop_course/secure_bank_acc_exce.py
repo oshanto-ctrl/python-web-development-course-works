b42ccdcb9d57ce6d3231e5089fd4ff5338499ce7
Rejoan Siddiky <siddikyrejoan@gmail.com>
2025-12-23 00:51:54 +0600

    secure_bank_acc_exercise solve

--- oop_course/secure_bank_acc_exce.py
+++ oop_course/secure_bank_acc_exce.py
@@ -64,7 +64,7 @@ while True:
 
 
 problem_2 = """
-Problem 3: Employee Management System
+Problem 2: Employee Management System
 Problem Statement:
 You need to implement an Employee class that securely manages employee details.
 Class Specifications:
@@ -93,7 +93,35 @@ S
 Output:  
 5500.0  
 5775.0  
- """
+"""
+class Employee:
+    def __init__(self, emp_id, emp_salary):
+        self.emp_id = emp_id
+        self.__emp_salary = emp_salary
+    
+    def increase_salary(self, percentage):
+        self.__emp_salary += self.__emp_salary * (percentage/100.00)
+    
+    def get_salary(self):
+        return self.__emp_salary
+
+# obj
+emp_id = input("Enter EMP ID").strip()
+salary = float(input("Enter base salary: "))
+employee = Employee(emp_id, salary)
+
+while True:
+    try:
+        query = input("[I]->Percentange\n[S]->Salary").strip()
+        if query.startswith("I"):
+            _, percent = query.split()
+            employee.increase_salary(float(percent))
+        elif query == "S":
+            print(employee.get_salary())
+    except EOFError:
+        break
+
+
 problem_three = """
 Problem 3: Smart Home System
 Problem Statement:
@@ -130,6 +158,81 @@ Devices: LivingRoomLight, HomeThermostat
 Light LivingRoomLight is now ON  
 Thermostat HomeThermostat is set to 25°C  
 Light LivingRoomLight is now OFF  
-
-
 """
+
+# Device class
+class Device:
+    def __init__(self, name):
+        self.name = name
+
+class Light(Device):
+    def __init__(self, name):
+        super().__init__(name)
+        self.is_on = False
+
+    def toggle(self):
+        self.is_on = not self.is_on
+        state = "ON" if self.is_on else "OFF"
+        print(f"Light {self.name} is now {state}.")
+
+class Thermostat(Device):
+    def __init__(self, name):
+        super().__init__(name)
+        self.temparature = None
+    
+    def set_temperature(self, value):
+        self.temparature = value
+        print(f"Thermostat {self.name} is set to {value} degree celcius.")
+
+# Smart home class
+class SmartHome:
+    def __init__(self):
+        self.devices = {}
+    
+    def add_device(self, device):
+        self.devices[device.name] = device
+    
+    def list_devices(self):
+        print("Devices: ", ", ".join(self.devices.keys()))
+    
+    def toggle_light(self, name):
+        device = self.devices.get(name)
+        if isinstance(device, Light):
+            device.toggle()
+    
+    def set_temperature(self, name, value):
+        device = self.devices.get(name)
+        if isinstance(device, Thermostat):
+            device.set_temperature(value)
+
+
+# obj
+home = SmartHome()
+created_devices = {}
+
+while True:
+    try:
+        command = input().strip()
+        parts = command.split()
+
+        if parts[0] == "Light":
+            created_devices[parts[1]] = Light(parts[1])
+
+        elif parts[0] == "Thermostat":
+            created_devices[parts[1]] = Thermostat(parts[1])
+
+        elif parts[0] == "Add":
+            home.add_device(created_devices[parts[1]])
+
+        elif parts[0] == "List":
+            home.list_devices()
+
+        elif parts[0] == "Toggle":
+            home.toggle_light(parts[1])
+
+        elif parts[0] == "SetTemp":
+            home.set_temperature(parts[1], int(parts[2]))
+
+    except EOFError:
+        break
+
